89fe7113c9e8a7f1e66cfe212cc4cafc425062b5
msanchezperez1 <msperez117@gmail.com>
2018-09-24 15:40:41 -0700

    Finished pure basic. Needs logo & connection[C

---
+++ LogInScreen/login.js
@@ -0,0 +1,8 @@
+function passwordLength() { //Supposed to opacitate the button
+    var x = document.getElementById("passwordLength");
+    if (x. "password") {
+        
+    } else {
+        x.type = "password";
+    }
+}
