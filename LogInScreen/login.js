13774a4da3f3139ce0fd6f59377c17621956ae36
msanchezperez1 <msperez117@gmail.com>
2018-10-03 19:26:44 -0700

    Finished pure basic. Needs logo & connection

--- LogInScreen/login.js
+++ LogInScreen/login.js
@@ -1,3 +1,22 @@
+
+// Firebase App is always required and must be first
+var firebase = require("firebase/app");
+
+// Add additional services that you want to use
+require("firebase/auth");
+require("firebase/database");
+require("firebase/firestore");
+require("firebase/messaging");
+require("firebase/functions");
+
+// Comment out (or don't require) services that you don't want to use
+// require("firebase/storage");
+
+var config = {
+    // ...
+};
+firebase.initializeApp(config);
+
 function passwordLength() { //Supposed to opacitate the button
     var x = document.getElementById("passwordLength");
     if (x. "password") {
@@ -6,3 +25,4 @@ function passwordLength() { //Supposed to opacitate the button
         x.type = "password";
     }
 }
+
